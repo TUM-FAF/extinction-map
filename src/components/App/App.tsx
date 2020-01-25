242e4bca7e3d1b284930a955de924dcfef70e379
Dragoș Străinu <str.dr4605@gmail.com>
2020-01-25 16:52:10 +0200

    create components, service folder

---
+++ src/components/App/App.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./App.css";
+
+export const App: React.FC = () => {
+  return <div className="App">hello world</div>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { parser } from './services/parser';
+import { App } from './components/App';
 import * as serviceWorker from './serviceWorker';
 
+parser();
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
