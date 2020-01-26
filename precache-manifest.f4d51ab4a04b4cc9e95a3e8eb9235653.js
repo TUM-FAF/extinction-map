395d949d4fc1fdf73b0cbf14957047ee4be8ea90
Dragoș Străinu <str.dr4605@gmail.com>
2020-01-26 03:47:08 +0200

    Updates

---
+++ precache-manifest.f4d51ab4a04b4cc9e95a3e8eb9235653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68a6ce2811d720b4ae60bf2935bf16db",
+    "url": "/extinction-map/index.html"
+  },
+  {
+    "revision": "6b9870d87c4644a6970c",
+    "url": "/extinction-map/static/css/main.00f1f014.chunk.css"
+  },
+  {
+    "revision": "1a56991ba4ea99f80e13",
+    "url": "/extinction-map/static/js/2.523899ae.chunk.js"
+  },
+  {
+    "revision": "ce3126c332c3bc8f1544c88c174f7c51",
+    "url": "/extinction-map/static/js/2.523899ae.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b9870d87c4644a6970c",
+    "url": "/extinction-map/static/js/main.55b55df4.chunk.js"
+  },
+  {
+    "revision": "ccb39abf2fc69ccbf6b4",
+    "url": "/extinction-map/static/js/runtime-main.527cfc5b.js"
+  }
+]);
